dc3b0557271bcd8763c19947e9745ba37b2f5302
Ogelo10 <augustineofoegbu1@gmail.com>
2021-11-17 19:33:33 +0100

    React quiz app

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import "./app.css";
+import { useState } from 'react';
+import Trivia from "./components/Trivia";
+function App() {
+
+  const [questionNumber, setquestionNumber] = useState(1);
+
+  const moneyPyramid = [
+    {id: 1, amount:"$ 100"},
+    {id: 2, amount:"$ 200"},
+    {id: 3, amount:"$ 300"},
+    {id: 4, amount:"$ 500"},
+    {id: 5, amount:"$ 1000"},
+    {id: 6, amount:"$ 2000"},
+    {id: 7, amount:"$ 4000"},
+    {id: 8, amount:"$ 8000"},
+    {id: 9, amount:"$ 16000"},
+    {id: 10, amount:"$ 32000"},
+    {id: 11, amount:"$ 64000"},
+    {id: 12, amount:"$ 125000"},
+    {id: 13, amount:"$ 250000"},
+    {id: 14, amount:"$ 500000"},
+    {id: 15, amount:"$ 1000000"}
+  ].reverse();
+
+  return (
+
+    <div className="app">
+
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">
+          <Trivia />
+        </div>      
+      </div>
+
+      <div className="pyramid">
+        <ul className="moneyList">
+          {moneyPyramid.map((m) => (
+            <li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"}>
+              <span className="moneyListItemNumber">{m.id}</span>
+              <span className="moneyListItemAmount">{m.amount}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      
+    </div>
+
+  );
+
+}
+
+export default App;
---
+++ src/components/Trivia.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Trivia() {
+    return (
+        <div className="trivia">
+            <div className="question">Who's the best software engineer?</div>
+            <div className="answers">
+                <div className="answer active">Ogelo</div>
+                <div className="answer">Ogelo</div>
+                <div className="answer">Ogelo</div>
+                <div className="answer">Ogelo</div>
+            </div>
+        </div>
+    )
+}
+
+export default Trivia
