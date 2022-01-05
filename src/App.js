24f1619c17593f0bb6097e8c7af614a82be55497
Ogelo10 <augustineofoegbu1@gmail.com>
2022-01-05 18:34:58 +0100

    Working React Millionaire Quiz App using Hooks

--- src/App.js
+++ src/App.js
@@ -1,42 +1,136 @@
 import "./app.css";
-import { useState } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Trivia from "./components/Trivia";
+import Timer from './Timer';
+import Start from './components/Start';
+
 function App() {
 
-  const [questionNumber, setquestionNumber] = useState(1);
+  const [username, setUsername] = useState(null);
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [stop, setStop] = useState(false);
+  const [earned, setEarned] = useState("$ 0");
 
-  const moneyPyramid = [
-    {id: 1, amount:"$ 100"},
-    {id: 2, amount:"$ 200"},
-    {id: 3, amount:"$ 300"},
-    {id: 4, amount:"$ 500"},
-    {id: 5, amount:"$ 1000"},
-    {id: 6, amount:"$ 2000"},
-    {id: 7, amount:"$ 4000"},
-    {id: 8, amount:"$ 8000"},
-    {id: 9, amount:"$ 16000"},
-    {id: 10, amount:"$ 32000"},
-    {id: 11, amount:"$ 64000"},
-    {id: 12, amount:"$ 125000"},
-    {id: 13, amount:"$ 250000"},
-    {id: 14, amount:"$ 500000"},
-    {id: 15, amount:"$ 1000000"}
-  ].reverse();
+  const data = [
+    {
+      id: 1,
+      question: "Who is the best software developer?",
+      answers: [
+        {
+          text: "Ogelo",
+          correct: true,
+        },
+        {
+          text: "Anthony",
+          correct: false,
+        },
+        {
+          text: "Ben",
+          correct: false,
+        },
+        {
+          text: "Chris",
+          correct: false,
+        }
+      ]
+    },
+    {
+      id: 1,
+      question: "Which is the best location?",
+      answers: [
+        {
+          text: "Abuja",
+          correct: false,
+        },
+        {
+          text: "Benin",
+          correct: false,
+        },
+        {
+          text: "Calabar",
+          correct: false,
+        },
+        {
+          text: "Delta",
+          correct: true,
+        }
+      ]
+    },
+    {
+      id: 1,
+      question: "Which is the technology track?",
+      answers: [
+        {
+          text: "Data Science",
+          correct: false,
+        },
+        {
+          text: "Web Development",
+          correct: true,
+        },
+        {
+          text: "Mobile App",
+          correct: false,
+        },
+        {
+          text: "Networking",
+          correct: false,
+        }
+      ]
+    },
+  ];
+
+  const moneyPyramid = useMemo(() => 
+    [
+      {id: 1, amount:"$ 100"},
+      {id: 2, amount:"$ 200"},
+      {id: 3, amount:"$ 300"},
+      {id: 4, amount:"$ 500"},
+      {id: 5, amount:"$ 1000"},
+      {id: 6, amount:"$ 2000"},
+      {id: 7, amount:"$ 4000"},
+      {id: 8, amount:"$ 8000"},
+      {id: 9, amount:"$ 16000"},
+      {id: 10, amount:"$ 32000"},
+      {id: 11, amount:"$ 64000"},
+      {id: 12, amount:"$ 125000"},
+      {id: 13, amount:"$ 250000"},
+      {id: 14, amount:"$ 500000"},
+      {id: 15, amount:"$ 1000000"}
+    ].reverse(),
+  []);
+
+  useEffect(() => {
+    questionNumber > 1 && setEarned(moneyPyramid.find(m => m.id  === questionNumber - 1).amount);
+    
+  }, [moneyPyramid, questionNumber]);
 
   return (
 
     <div className="app">
+      {username ? (
+        <>
+          <div className="main">
 
-      <div className="main">
-        <div className="top">
-          <div className="timer">30</div>
+        {stop ? <h1 className="endText">You earned: {earned}</h1> : (
+        <>
+          <div className="top">
+            <div className="timer">
+              <Timer setStop={setStop} questionNumber={questionNumber} />
+            </div>
+          </div>
+          <div className="bottom">
+            <Trivia 
+              data={data} 
+              setStop={setStop} 
+              questionNumber={questionNumber}
+              setQuestionNumber={setQuestionNumber} />
+          </div>   
+        </>
+        )}   
         </div>
-        <div className="bottom">
-          <Trivia />
-        </div>      
-      </div>
 
-      <div className="pyramid">
+        <div className="pyramid">
         <ul className="moneyList">
           {moneyPyramid.map((m) => (
             <li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"}>
@@ -45,7 +139,10 @@ function App() {
             </li>
           ))}
         </ul>
-      </div>
+        </div>
+        </>
+      ) : <Start setUsername={setUsername}  /> }
+      
       
     </div>
 
